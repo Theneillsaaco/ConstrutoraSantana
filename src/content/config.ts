dee568eb5fa2351cbb26ff26ff90b55c84012ecc
Theneillsaaco <isaacdepena18@gmail.com>
2025-12-20 17:00:05 -0400

    Add FeaturedProjects Add images in text.md

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.union([z.string(), z.number()]),
+    type: z.string(),
+    location: z.string(),
+    tags: z.array(z.string()),
+    img: z.object({
+      cover: z.string(),
+      others: z.array(z.string()).optional()
+    })
+  })
+});
+
+export const collections = { projects };
