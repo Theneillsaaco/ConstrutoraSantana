ab9a89653df158e3a33872ee77ace072c70fccad
Theneillsaaco <isaacdepena18@gmail.com>
2026-07-09 21:04:50 -0400

    Add new clients to the portfolio list

--- src/content/Clients/clients.ts
+++ src/content/Clients/clients.ts
@@ -20,5 +20,39 @@ export const clients: Client[] = [
     logo: "/ConstrutoraSantana/Images/Clients/Company/Remix.png",
     url: "https://remixsa.do/",
   },
-
+  {
+    name: "Unibe",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/Unibe.png",
+    url: "https://www.unibe.edu.do/",
+  },
+  {
+    name: "NexusTours",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/NexusTours.svg",
+    url: "https://www.nexustours.com/",
+  },
+  {
+    name: "TargetGroup",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/TargetGroup.png",
+    url: "http://targetgroupdr.com/",
+  },
+  {
+    name: "ContructoraDupla",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/Dupla.png",
+    url: "https://grupodupla.com/",
+  },
+  {
+    name: "CivilMek",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/CivilMek.png",
+    url: "https://www.civilmek.com/",
+  },
+  {
+    name: "AceroEstrella",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/AceroEstrella.png",
+    url: "https://estrella.com.do/empresas/acero-estrella/",
+  },
+  {
+    name: "Codelpa",
+    logo: "/ConstrutoraSantana/Images/Clients/Company/Codelpa.png",
+    url: "https://codelpa.com/",
+  }
 ];
